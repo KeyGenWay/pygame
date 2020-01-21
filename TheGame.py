048e3f6c5d4a843855054c8e342fbc1a8df9bd52
Maciej <keygenmen@gmail.com>
2020-01-21 23:15:55 +0100

    Third Version, added algorithm to run away or chase player. Added detection of player in specified range Still missing collision between rocks

--- TheGame.py
+++ TheGame.py
@@ -2,8 +2,8 @@ import pygame
 import random
 import keyboard
 
-WIDTH = 400
-HEIGHT = 400
+WIDTH = 800
+HEIGHT = 800
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -11,12 +11,33 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+
+
 HIGH_SPEED = 4
+#MID_SPEED = 3 Aktualnie nie wykorzystywane, moze zostac uzyte aby uplynnic poruszanie sie gracza
 LOW_SPEED = 2
 NO_SPEED = 0
 
-FPS = 240
-
+FPS = 60
+# Przenikanie ze sciany na sciane
+def moveToOtherSide(self):
+    if self.rect.left > WIDTH:
+        self.rect.right = 0
+    if self.rect.right < 0:
+        self.rect.left = WIDTH
+    if self.rect.top > HEIGHT:
+        self.rect.bottom = 0
+    if self.rect.bottom < 0:
+        self.rect.top = HEIGHT
+def collideWithBorder(self):
+    if self.rect.right > WIDTH:
+        self.rect.right = WIDTH
+    if self.rect.left < 0:
+        self.rect.left = 0
+    if self.rect.bottom > HEIGHT:
+        self.rect.bottom = HEIGHT
+    if self.rect.top < 0:
+        self.rect.top = 0
 class Player(pygame.sprite.Sprite):
     angle = 0
 
@@ -35,14 +56,14 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH / 2, HEIGHT / 2)
         pygame.draw.line(self.original_surface, RED, (25,25), (25,0), 4)
-        pygame.draw.circle(self.original_surface, RED, (25, 25), 25, 4)
+        pygame.draw.circle(self.original_surface, RED, (25, 25), 30, 4)
     def update(self):
 
         # Aktualizacja pozycji przy kliknieciu
         if keyboard.is_pressed("down_arrow"):
-            self.rect.y += 1
+            self.move(True)
         if keyboard.is_pressed("up_arrow"):
-            self.moveForward()
+            self.move(False)
         if keyboard.is_pressed("left_arrow"):
             self.image = pygame.transform.rotate(self.original_surface, self.angle)
             self.updateAngle(4)
@@ -56,76 +77,64 @@ class Player(pygame.sprite.Sprite):
             self.rect = self.image.get_rect()
             self.rect.center = (x, y)
 
-
         # Przenikanie ze sciany na sciane
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
-        if self.rect.right < 0:
-            self.rect.left = WIDTH
-        if self.rect.top > HEIGHT:
-            self.rect.bottom = 0
-        if self.rect.bottom < 0:
-            self.rect.top = HEIGHT
+        moveToOtherSide(self)
 
-        #Kolizja z kamieniem (prost wersja na punktach srodkowych)
-        if self.rect.collidepoint(rock.rect.midtop):
-            self.rect.bottom = rock.rect.top
-        if self.rect.collidepoint(rock.rect.midleft):
-            self.rect.right = rock.rect.left
-        if self.rect.collidepoint(rock.rect.midright):
-            self.rect.left = rock.rect.right
-        if self.rect.collidepoint(rock.rect.midbottom):
-            self.rect.top = rock.rect.bottom
-    def moveForward(self):
+    def move(self, backward):
+        direction = self.angle;
+        if backward:
+            direction = direction - 180
+            if direction < 0:
+                direction = 360 + direction
         # Zmiana pozycji przy poruszaniu sie w kierunku  w  konkretnych cwiartkach
-        if 0 < self.angle < 45:
+        if 0 < direction < 45:
             self.rect.x -= LOW_SPEED
             self.rect.y -= HIGH_SPEED
-        if 45 < self.angle < 90:
+        if 45 < direction < 90:
             self.rect.x -= HIGH_SPEED
             self.rect.y -= LOW_SPEED
-        if 90 < self.angle < 135:
+        if 90 < direction < 135:
             self.rect.x -= HIGH_SPEED
             self.rect.y += LOW_SPEED
-        if 135 < self.angle < 180:
+        if 135 < direction < 180:
             self.rect.x -= LOW_SPEED
             self.rect.y += HIGH_SPEED
-        if 180 < self.angle < 225:
+        if 180 < direction < 225:
             self.rect.x += LOW_SPEED
             self.rect.y += HIGH_SPEED
-        if 225 < self.angle < 270:
+        if 225 < direction < 270:
             self.rect.x += HIGH_SPEED
             self.rect.y += LOW_SPEED
-        if 270 < self.angle < 315:
+        if 270 < direction < 315:
             self.rect.x += HIGH_SPEED
             self.rect.y -= LOW_SPEED
-        if 315 < self.angle < 460:
+        if 315 < direction < 460:
             self.rect.x += LOW_SPEED
             self.rect.y -= HIGH_SPEED
 
         ## Wartosci progowe w cwiartkach
-        if self.angle == 0:
+        if direction == 0:
             self.rect.x -= NO_SPEED
             self.rect.y -= HIGH_SPEED
-        if self.angle == 45:
+        if direction == 45:
             self.rect.x -= HIGH_SPEED
             self.rect.y -= HIGH_SPEED
-        if self.angle == 90:
+        if direction == 90:
             self.rect.x -= HIGH_SPEED
             self.rect.y -= NO_SPEED
-        if self.angle == 135:
+        if direction == 135:
             self.rect.x -= HIGH_SPEED
             self.rect.y += HIGH_SPEED
-        if self.angle == 180:
+        if direction == 180:
             self.rect.x += NO_SPEED
             self.rect.y += HIGH_SPEED
-        if self.angle == 225:
+        if direction == 225:
             self.rect.x += HIGH_SPEED
             self.rect.y += HIGH_SPEED
-        if self.angle == 270:
+        if direction == 270:
             self.rect.x += HIGH_SPEED
             self.rect.y -= 0
-        if self.angle == 315:
+        if direction == 315:
             self.rect.x += HIGH_SPEED
             self.rect.y -= HIGH_SPEED
 
@@ -138,20 +147,100 @@ class Rock(pygame.sprite.Sprite):
     __dx = 0
     __dy = 0
     __cycle = 60*FPS
+    detectRange = 200
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((random.randint(30, 60), random.randint(30, 60)))
-        self.image.fill(BLACK)
+        self.image = pygame.Surface((50, 50))
         self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(2, WIDTH), random.randint(2, HEIGHT))
+        self.image.fill(WHITE)
+        self.rect.center = (random.randint(1, WIDTH), random.randint(1, HEIGHT))
+        pygame.draw.circle(self.image, GREEN, (25,25), 15, 15)
     def update(self):
-        self.rect.x += self.__dx
-        self.rect.y += self.__dy
-        if self.__cycle == 0:
-            self.__dx = random.randint(-2, 2)
-            self.__dy = random.randint(-2, 2)
-            self.__cycle = 60*FPS
-        self.__cycle -= FPS
+        #self.rect.x += self.__dx
+        #self.rect.y += self.__dy
+        #if self.__cycle == 0:
+        #    self.__dx = random.randint(-2, 2)
+        #    self.__dy = random.randint(-2, 2)
+        #    self.__cycle = 60*FPS
+        #self.__cycle -= FPS
+        self.collideWithPlayer()
+        self.detectAndRunAwayFromPlayer()
+        collideWithBorder(self)
+        self.collideWithSecondRock()
+    def collideWithPlayer(self):
+        #Kolizja z graczem (prost wersja na punktach srodkowych)
+        if self.rect.collidepoint(player.rect.midtop):
+            self.rect.y -= HIGH_SPEED
+        if self.rect.collidepoint(player.rect.midbottom):
+            self.rect.y += HIGH_SPEED
+        if self.rect.collidepoint(player.rect.midleft):
+            self.rect.x -= HIGH_SPEED
+        if self.rect.collidepoint(player.rect.midright):
+            self.rect.x += HIGH_SPEED
+        #Kolizja na punktach katowych
+        if self.rect.collidepoint(player.rect.topleft):
+            self.rect.y -= HIGH_SPEED
+            self.rect.x -= HIGH_SPEED
+        if self.rect.collidepoint(player.rect.topright):
+            self.rect.y -= HIGH_SPEED
+            self.rect.x += HIGH_SPEED
+        if self.rect.collidepoint(player.rect.bottomleft):
+            self.rect.y += HIGH_SPEED
+            self.rect.x -= HIGH_SPEED
+        if self.rect.collidepoint(player.rect.bottomright):
+            self.rect.y += HIGH_SPEED
+            self.rect.x += HIGH_SPEED
+    def collideWithSecondRock(self):
+         collidedSprite = self.rect.collidedict(allSprites.spritedict)
+         print("Collision")
+         print(collidedSprite[0].rect.center)
+    def detectAndGoAfterPlayer(self):
+        # Ponizszy kod zamiast uciekac goni gracza
+        if abs(self.rect.center[0] - player.rect.center[0]) < self.detectRange and abs(self.rect.center[1] - player.rect.center[1]) < self.detectRange:
+            if self.rect.x > player.rect.x:
+                if self.rect.y > player.rect.y:
+                    #print("He is left above!")
+                    self.rect.y -= HIGH_SPEED
+                    self.rect.x -= HIGH_SPEED
+                else:
+                    #print("He is left below!")
+                    self.rect.y += HIGH_SPEED
+                    self.rect.x -= HIGH_SPEED
+            else:
+                if self.rect.y > player.rect.y:
+                    #print("He is right above!")
+                    self.rect.y -= HIGH_SPEED
+                    self.rect.x += HIGH_SPEED
+                else:
+                    #print("He is right below!")
+                    self.rect.y += HIGH_SPEED
+                    self.rect.x += HIGH_SPEED
+
+    def detectAndRunAwayFromPlayer(self):
+        # Uciekanie przed graczem
+        if abs(self.rect.center[0] - player.rect.center[0]) < self.detectRange and abs(
+                self.rect.center[1] - player.rect.center[1]) < self.detectRange:
+            if self.rect.x > player.rect.x:
+                if self.rect.y > player.rect.y:
+                    #print("He is left above!")
+                    self.rect.y += LOW_SPEED
+                    self.rect.x += LOW_SPEED
+                else:
+                    #print("He is left below!")
+                    self.rect.y -= LOW_SPEED
+                    self.rect.x += LOW_SPEED
+            else:
+                if self.rect.y > player.rect.y:
+                    #print("He is right above!")
+                    self.rect.y += LOW_SPEED
+                    self.rect.x -= LOW_SPEED
+                else:
+                    #print("He is right below!")
+                    self.rect.y -= LOW_SPEED
+                    self.rect.x -= LOW_SPEED
+
+
+
 
 pygame.init()
 pygame.mixer.init()
@@ -165,8 +254,8 @@ generator = RockGenerator()
 rock = Rock()
 allSprites.add(player)
 allSprites.add(rock)
-#for i in range(10):
-#   allSprites.add(generator.generateRock())
+for i in range(10):
+   allSprites.add(generator.generateRock())
 running = True
 
 while running:
